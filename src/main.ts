c188bd7a101bfe96ab10966b8977046f08de7726
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-10-30 18:53:02 -0700

    add aws amplify auth

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,31 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { Amplify } from 'aws-amplify';
+import { default as config } from './aws-exports';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const isLocalhost = !!(window.location.hostname === 'localhost');
+
+const [productionRedirectSignIn, localRedirectSignIn] =
+  config.oauth.redirectSignIn.split(',');
+const [productionRedirectSignOut, localRedirectSignOut] =
+  config.oauth.redirectSignOut.split(',');
+
+const updatedAwsConfig = {
+  ...config,
+  oauth: {
+    ...config.oauth,
+    redirectSignIn: isLocalhost
+      ? localRedirectSignIn
+      : productionRedirectSignIn,
+    redirectSignOut: isLocalhost
+      ? localRedirectSignOut
+      : productionRedirectSignOut,
+  },
+};
+
+Amplify.configure(updatedAwsConfig);
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
