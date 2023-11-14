e2337400f9662c4f3cbeeb55762b12181ff835fd
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-11-13 17:02:11 -0800

    add login logout in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,16 @@
 import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
 import { LocationService } from './location.service';
-import { AuthService } from './auth.service';
-import { CognitoUser } from 'amazon-cognito-identity-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   isLoading = false;
-  isAuthenticated = false;
-  username = '';
-  user: any = null;
 
-  constructor(
-    private locationService: LocationService,
-    private auth: AuthService,
-    private zone: NgZone
-  ) {
+  constructor(private locationService: LocationService) {
     this.locationService.isLoading$.subscribe((isLoading) => {
       this.isLoading = false;
     });
-    this.auth.user.subscribe((user) => {
-      this.zone.run(() => {
-        console.log('user:', user);
-        this.user = user;
-        this.username = this.user?.attributes?.email;
-        this.isAuthenticated = !!user;
-      });
-    });
-  }
-
-  async signInWithGoogle() {
-    const socialResult = await this.auth.googleSocialSignIn();
-    console.log('google Result:', socialResult);
-  }
-
-  async signOut() {
-    await this.auth.signOut();
   }
 
   ngOnInit(): void {}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, ViewChild } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  @ViewChild('dropdown') dropdown: any;
+  isLoading = false;
+  isAuthenticated = false;
+  email = '';
+  user: any = null;
 
+  constructor(private auth: AuthService, private zone: NgZone) {
+    this.auth.user.subscribe((user) => {
+      this.zone.run(() => {
+        console.log('user:', user);
+        this.user = user;
+        this.email = this.user?.attributes?.email;
+        this.isAuthenticated = !!user;
+      });
+    });
+  }
+
+  async signInWithGoogle() {
+    const socialResult = await this.auth.googleSocialSignIn();
+    console.log(socialResult);
+  }
+
+  async signOut() {
+    await this.auth.signOut();
+  }
+
+  toggleDropdown() {
+    if (this.dropdown) {
+      this.dropdown.nativeElement.classList.toggle('hidden');
+    }
+  }
 }
